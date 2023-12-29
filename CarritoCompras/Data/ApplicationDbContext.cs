7f5a6440bd2814f7099712087a6a788533c286d8
Andres <andresr0428@gmail.com>
2023-12-28 19:10:54 -0500

    Models

--- CarritoCompras/Data/ApplicationDbContext.cs
+++ CarritoCompras/Data/ApplicationDbContext.cs
@@ -1,8 +1,48 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarritoCompras.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarritoCompras.Data
 {
     public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Rol> Roles { get; set; } = null!;
+        public DbSet<Producto> Productos { get; set; } = null!;
+        public DbSet<Pedido> Pedidos { get; set; } = null!;
+        public DbSet<Detalle_Pedido> DetallePedidos { get; set; } = null!;
+        public DbSet<Categoria> Categorias { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>()
+            .HasMany(u => u.Pedidos)
+            .WithOne(p => p.Usuario)
+            .HasForeignKey(p => p.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Producto>()
+            .HasMany(p => p.DetallesPedido)
+            .WithOne(dp => dp.Producto)
+            .HasForeignKey(dp => dp.ProductoId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Pedido>()
+            .HasMany(p => p.DetallesPedido)
+            .WithOne(dp => dp.Pedido)
+            .HasForeignKey(dp => dp.PedidoId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Categoria>()
+            .HasMany(c => c.Productos)
+            .WithOne(p => p.Categoria)
+            .HasForeignKey(p => p.CategoriaId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        }
     }
 }
